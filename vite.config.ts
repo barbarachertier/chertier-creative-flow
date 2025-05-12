2dbb66af9ea884ece7c130a75c85dae7dc23e15e
Barbara Chertier <barbarachertier1@gmail.com>
2025-05-12 15:31:27 +0200

    test de la queen

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/chertier-creative-flow/", // 👈 REQUIRED for GitHub Pages
+base: "/chertier-creative-flow/",
+ // 👈 REQUIRED for GitHub Pages
 
   server: {
     host: "::",
