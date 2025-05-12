22876c5fcc5d09dc94d0876dbd14faa1eaeb3089
barbarachertier <84504890+barbarachertier@users.noreply.github.com>
2025-05-12 14:44:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/chertier-creative-flow/", // 👈 REQUIRED for GitHub Pages
-
+  base: "/chertier-creative-flow/", // ← Très important !!
   server: {
     host: "::",
     port: 8080,
   },
-
   plugins: [
     react(),
     mode === "development" && componentTagger(),
   ].filter(Boolean),
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
