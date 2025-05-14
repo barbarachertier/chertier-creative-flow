83313741154287f248936a7ac1d5df08828a7680
Barbara Chertier <barbarachertier1@gmail.com>
2025-05-14 10:09:24 +0200

    Refactor: Clean up index.html, update image path in AboutSection, enhance PortfolioSection with animation classes, and streamline main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import "@fontsource/dancing-script";
-
-// Make sure we have a valid root element
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-  console.error("Failed to find the root element");
-  throw new Error("Failed to find the root element");
-}
-
-const root = createRoot(rootElement);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
